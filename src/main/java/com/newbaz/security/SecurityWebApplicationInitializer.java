9deaa95cfed33f0ac1afbbaf87e900f94b89f326
Dorsa <dorsapiri@gmail.com>
2017-04-05 11:56:36 +0430

    remove target file

--- src/main/java/com/newbaz/security/SecurityWebApplicationInitializer.java
+++
@@ -1,9 +0,0 @@
-package com.newbaz.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-/**
- * Created by dorsa on 1/7/17.
- */
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
